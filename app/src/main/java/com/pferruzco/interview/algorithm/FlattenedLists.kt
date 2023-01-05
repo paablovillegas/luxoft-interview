385e9743b3b33000a0d32db9d108fc7614279d38
Pablo Villegas <v-pferruzco@expediagroup.com>
2023-01-05 15:28:13 -0600

    First two challenges added

---
+++ app/src/main/java/com/pferruzco/interview/algorithm/FlattenedLists.kt
@@ -0,0 +1,13 @@
+package com.pferruzco.interview.algorithm
+
+/*
+Given an array of arrays
+Return a flattened array with all values except nil/null
+EG:
+    Input = [[1],[2,3,null,4], [5,6]]
+    Output = [1,2,3,4,5,6]
+*/
+fun flattenedList(list: Array<IntArray>): IntArray {
+    //TODO: Write your code here!
+    return intArrayOf()
+}
---
+++ app/src/main/java/com/pferruzco/interview/code/CaesarCipher.kt
@@ -0,0 +1,16 @@
+package com.pferruzco.interview.code
+
+
+/*
+Given a phrase, we want it to be encrypted with a rotational cipher n by shifting each letter n times.
+    Input = {
+        phrase = “Hello World”
+        n = 4
+    }
+    Ouput = “Lipps Asvph”
+Tip: The alphabet has 26 letters
+*/
+fun caesarCipher(message: String, n: Int): String {
+    //TODO: write your code here!
+    return ""
+}
