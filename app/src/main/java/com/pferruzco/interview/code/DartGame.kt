7df4e820ba2220e0232534a64fc5749692097ef0
Pablo Villegas <v-pferruzco@expediagroup.com>
2023-01-05 16:52:27 -0600

    New code challenge added

---
+++ app/src/main/java/com/pferruzco/interview/code/DartGame.kt
@@ -0,0 +1,20 @@
+package com.pferruzco.interview.code
+
+import kotlin.math.sqrt
+
+/*
+Write a function that returns the earned points in a dart game
+Given the coordinate (x, y):
+If the dart lands outside the target = 0 pts
+If the dart lands in the outer circle = 1 pts
+If the dart lands in the middle circle = 5 pts
+If the dart lands in the inner circle = 10 pts
+
+The outer circle has a radius of 10 units
+The middle circle has a radius of 5 units
+The inner circle has a radius of 1 unit
+*/
+fun calculatePoints(coordinates: Pair<Double, Double>): Int {
+    //TODO: write your code here!
+    return 0
+}
---
+++ app/src/test/java/com/pferruzco/interview/code/DartGameTest.kt
@@ -0,0 +1,43 @@
+package com.pferruzco.interview.code
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+internal class DartGameTest {
+
+    @Test
+    fun `Dart out of board`() {
+        val coordinates = Pair(-10.0, -10.0)
+        val points = calculatePoints(coordinates)
+        val expectedResult = 0
+
+        assertEquals(points, expectedResult)
+    }
+
+    @Test
+    fun `Dart in outer circle`() {
+        val coordinates = Pair(5.0, 6.0)
+        val points = calculatePoints(coordinates)
+        val expectedResult = 1
+
+        assertEquals(points, expectedResult)
+    }
+
+    @Test
+    fun `Dart in middle circle`() {
+        val coordinates = Pair(1.0, -3.0)
+        val points = calculatePoints(coordinates)
+        val expectedResult = 5
+
+        assertEquals(points, expectedResult)
+    }
+
+    @Test
+    fun `Dart in inner circle`() {
+        val coordinates = Pair(-0.9, 0.4)
+        val points = calculatePoints(coordinates)
+        val expectedResult = 10
+
+        assertEquals(points, expectedResult)
+    }
+}
