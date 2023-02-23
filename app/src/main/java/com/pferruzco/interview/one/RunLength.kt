93d507d707022756f2591e3286192342d3adac6a
Pablo Villegas <39629252+paablovillegas@users.noreply.github.com>
2023-02-23 10:21:32 -0600

    Comment fixed in RunLength.kt

--- app/src/main/java/com/pferruzco/interview/one/RunLength.kt
+++ app/src/main/java/com/pferruzco/interview/one/RunLength.kt
@@ -4,8 +4,8 @@ package com.pferruzco.interview.one
 Run-length encoding (RLE) is a simple form of data compression, where runs (consecutive data
 elements) are replaced by just one data value and count.
 
-For example we can represent the original 53 characters with only 13.
-"AABCCCDEEEE"  ->  "2AB3CD4E"
+For example we can represent the original 16 characters with only 9.
+"AABCCCDEEEEAAAAA"  ->  "2AB3CD4E5A"
 
 RLE allows the original data to be perfectly reconstructed from the compressed data,
 which makes it a lossless data compression.
